db38f664dc09812957370c3eeb8c82cc9302ed78
Renato Lima <limar@sureskills.com>
2019-05-23 11:42:47 +0100

    second batch test

---
+++ index.js
@@ -0,0 +1,17 @@
+// Import library to create component
+import React from 'react';
+import { AppRegistry, View } from 'react-native';
+import Header from './src/components/Header';
+import AlbumList from './src/components/AlbumList';
+
+// Create component
+const App = () => (
+    <View style={{ flex: 1 }}>
+        <Header headerText={'Albums'} />
+        <AlbumList />
+    </View>
+);
+
+
+// Render it to device
+AppRegistry.registerComponent('albums', () => App);
